71f55f00ace536eb4b73d7567409dbb71a1f57f2
KennyphilS <59595976+KennyphilS@users.noreply.github.com>
2020-01-07 14:57:33 +0800

    Backup
    Para naay backup

--- TCEFUS/TCEFUS.ino
+++ TCEFUS/TCEFUS.ino
@@ -1,9 +1,12 @@
+#include "SERVO.h" // GITAWAG ANG SERVO NGA HEADER
+ServoMove tce;
+
 void setup() {
   // put your setup code here, to run once:
-
+  tce.servomove_setup();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  tce.servomove_loop();  
 }

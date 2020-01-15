890cb0cb4cf3adf9b26e6d6ef88eceda24d0db0b
Engr. Max Angelo Perin <perinmaks@outlook.ph>
2020-01-15 16:33:24 +0800

    CHANGED

--- TCEFUS/TCEFUS.ino
+++ TCEFUS/TCEFUS.ino
@@ -1,12 +1,18 @@
-#include "SERVO.h" // GITAWAG ANG SERVO NGA HEADER
+#include "SERVO.h"          // GITAWAG ANG SERVO NGA HEADER
+#include "TEMPERATURE.H"    // GITAWAG ANG TEMPERATURE NGA HEADER
 ServoMove tce;
+TemperatureDetect td;
 
 void setup() {
   // put your setup code here, to run once:
   tce.servomove_setup();
+  td.temperatureselect_setup();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   tce.servomove_loop(80);  
+    Serial.print("temperature = ");
+    Serial.print(td.temperatureselect_loop); 
+    Serial.println("C  ");
 }
